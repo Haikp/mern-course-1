533d2ab5b5c91c2fa0097af0d20a8d80047013cc
Kelvin Do <kelvin@Kelvins-MacBook-Air.local>
2025-05-18 21:59:09 -0700

    backend completed

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import Product from "./models/product.model.js";
+import proudctRoutes from "./routes/product.route.js";
 
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); //allows us to take JSON data in the req.body
 
-app.post("/api/products", async (req, res) => {
-    const product = req.body; // user will send this data
+app.use("/api/products", proudctRoutes);
 
-    if (!product.name || !product.price || !product.image) {
-        return res.status(400).json({success: false, message: "Please provide all fields"});
-    }
-
-    const newProduct = new Product(product);
-
-    try {
-        await newProduct.save();
-        res.status(201).json({success: true, data: newProduct});
-    } catch (error) {
-        console.error("Error in Create product: ", error.message);
-        res.status(500).json({success: false, message: "Server Error"});
-    }
-});
-
-app.listen(3000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log('Server started at http://localhost:3000');
+    console.log("Server started at http://localhost:" + PORT);
 });
